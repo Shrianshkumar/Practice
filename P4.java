281642a4fd1f07c97c5f7ccb28fc8766863ce181
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2023-02-25 23:19:27 +0530

    Pattern 5

---
+++ P4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class P4 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n=scn.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int s=n-i;s>=1;s--){
+                System.out.print("\t");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*\t");
+            }
+        System.out.println();
+    }
+    }
+}
