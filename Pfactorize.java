6e38af0908528d22e22a4f2319750a6b416ce676
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2023-02-25 23:15:09 +0530

    Prime Factorization

---
+++ Pfactorize.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Pfactorize{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        for(int div=2; div * div <=num; div++){
+            while(num % div ==0){
+                num /= div;
+                System.out.print(div + " ");
+            }
+        }
+        if(num != 1){
+            System.out.print(num);
+        }
+    }
+}
